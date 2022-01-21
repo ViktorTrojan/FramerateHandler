499ae60bfd5103c9280de41558e22839a3d2328d
ViktorTrojan <87524553+ViktorTrojan@users.noreply.github.com>
2022-01-21 17:58:05 +0100

    added latency handling

--- src/FramerateHandler.java
+++ src/FramerateHandler.java
@@ -3,27 +3,35 @@ import java.awt.GraphicsEnvironment;
 public class FramerateHandler {
 
     public static boolean RUNNING = true;
-    // UPS = updates per second should be the same for everyone
-    public final static int UPS = 60;
-    // FPS = frames per second depends on the monitor refresh rate
-    public static int FPS;
+
+    public final static int UPS = 60;  // UPS = updates per second should be the same for everyone
+    public static int FPS; // FPS = frames per second depends on the monitor refresh rate
     public int frames, updates;
     public long lastTime, timer;
     public final double ns;
     double delta;
 
     public FramerateHandler() {
-        // fetch refreshrate from monitor-
+        // fetch refreshrate from monitor
         FPS = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[0].getDisplayMode().getRefreshRate();
         ns = 1000000000d / (double) this.UPS;
         timer = System.currentTimeMillis();
         lastTime = System.nanoTime();
-        // messure the actual fps count
+
         frames = 0;
-        // messure the actual ups count
         updates = 0;
-        // time difference between the current and last frame
         delta = 0;
+    }
+
+    // PUT YOUR UPDATE LOGIC HERE !
+    private void update() {
+        Main.draw.updateCubes();
+    }
+
+    // PUT YOUR RENDER LOGIC HERE !
+    private void render() {
+        // drawing gets processed here
+        Main.draw.redraw();
     }
 
     public void run() {
@@ -31,31 +39,36 @@ public class FramerateHandler {
             long now = System.nanoTime();
             delta += (now - lastTime) / ns;
             lastTime = now;
+
+            long dur = System.nanoTime();
             while (delta >= 1) { // updates get processed here
-                Main.draw.updateCubes();
+                update();
                 updates++;
                 delta--;
             }
-            
-            // drawing gets processed here
-            Main.draw.redraw();
-            frames++;
 
+            render();
+
+            int latency = (int) ((System.nanoTime() - dur) / (1000000d));
+            sleep(1000 / FPS - latency);
+
+            frames++;
             if (System.currentTimeMillis() - timer > 1000) {
-                System.out.println(updates + " ups, " + frames + " fps");
+                System.out.println(updates + " ups, " + frames + " fps" + " latency: " + latency + "ms");
                 updates = 0;
                 frames = 0;
                 timer = System.currentTimeMillis();
             }
-            
-            sleep(1000 / FPS);
         }
     }
 
     public void sleep(int ms) {
+        if (ms < 0) ms = 0;
         try {
             Thread.sleep(ms);
         } catch (InterruptedException e) {
+            // restore interrupted thread
+            Thread.currentThread().interrupt();
         }
     }
 }
